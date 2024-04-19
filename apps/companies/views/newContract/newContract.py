6bb31069c8d6faad4cc47019ca337b86aabb4192
mikeberdugo <manuel.david.13.b@gmail.com>
2024-04-19 16:54:08 -0500

    .

--- apps/companies/views/newContract/newContract.py
+++ apps/companies/views/newContract/newContract.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from apps.companies.models import Tipodocumento , Paises , Ciudades , Contratosemp,Profesiones
 
-def newContract(request):
+def newContractVisual(request):
     
-    return render(request, './companies/NewEmployee.html')
+    return render(request, './companies/newContractVisual.html')
+
+def newContractCreater(request,idempleado):
+    
+    
+    return render(request, './companies/newContractCreate.html')
