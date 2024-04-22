b8e7437e8208e5e5bc9d25c0ae2c0698e1bdb79e
nomiwebPro <sat1124@proton.me>
2024-04-22 09:03:35 -0500

    PRUEBA GITK

--- apps/companies/urls.py
+++ apps/companies/urls.py
@@ -7,23 +7,21 @@ from .views.newContract import newContract
 
 
 urlpatterns = [
-    ##todo novedades de nomina 
+    ##todo novedades de nomina
     path('companies/contract', ContractsList.startCompanies , name='startcompanies'),
-    
+
     path('companies/new/employee', newEmployee.newEmployee , name='newemployee'),
     path('companies/edit/employee',  EditEmployee.EditEmployeeSearch , name='editemployeesearch'),
     path('companies/edit/employee/<str:idempleado>',  EditEmployee.EditEmployeeVisual , name='editemployeevisual'),
-    
+
     path('companies/new/contract',newContract.newContractVisual ,name='newcontractvisual'),
     path('companies/new/contract/<str:idempleado>',newContract.newContractCreater ,name='newcontractcreater'),
-    
+
     path('companies/edit/contract',editContract.EditContracsearch , name='editcontracsearch'),
     path('companies/edit/contract/<str:idempleado>',editContract.EditContracVisual , name='editcontracvisual'),
-    
-    
-    
-    ##! empleados 
-    
+
+    ##! empleados
+
     ##! parametros 
     
     ##! contabilidad 
