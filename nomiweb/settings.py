85f7aa33b1441aaa98fb0bb0eb23f0415f6fb969
mikeberdugo <manuel.david.13.b@gmail.com>
2024-05-02 17:31:58 -0500

    .

--- nomiweb/settings.py
+++ nomiweb/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'apps.login.middlewares.DatabaseRouterMiddleware',
+    'apps.login.middlewares.DatabaseRouterMiddleware',
 ]
 
 
