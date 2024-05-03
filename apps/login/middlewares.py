e0820b4c2e610f1fed6eddd0d2a8c33a94ad9767
mikeberdugo <manuel.david.13.b@gmail.com>
2024-05-03 14:28:51 -0500

    Router db corregido
    falla dentro de los formularios , el llamado debe corregirse

--- apps/login/middlewares.py
+++ apps/login/middlewares.py
@@ -4,8 +4,18 @@ from django.urls import reverse
 
 class DatabaseRouterMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        singleton = NombreDBSingleton()
-        
+        try:
+            singleton = NombreDBSingleton()
+            if request.user.is_authenticated:
+                if 'usuario' in request.session and 'db' in request.session['usuario'] and not request.path == reverse('login:logout') and not request.path == reverse('login:login'):
+                    print("Middleware ejecutándose en una solicitud GET")
+                    db_name = request.session['usuario']['db']
+                    singleton.set_nombre_db(db_name)
+            else:
+                singleton.set_nombre_db('default')
+        except Exception as e:
+            print("Error en el middleware de enrutamiento de base de datos:", e)
+            
         # request.session.clear()
         # if request.user.is_authenticated:
         #     print(request.session)
@@ -16,16 +26,16 @@ class DatabaseRouterMiddleware(MiddlewareMixin):
         # if  request.method == 'GET' and singleton.get_nombre_db() == "default" and not request.path == reverse('login:logout') and not request.path == reverse('login:login') :
         #     print("Middleware ejecutándose en una solicitud GET")
         #     singleton.set_nombre_db('lectaen')
-        if  (request.path == reverse('login:login')  or request.path == reverse('login:logout') ) and request.method == 'POST':
-            print("Middleware ejecutándose en una solicitud GET")
-            singleton.set_nombre_db('default')
-        else : 
-            singleton.set_nombre_db('lectaen')
+        # if  (request.path == reverse('login:login')  or request.path == reverse('login:logout') ) and request.method == 'POST':
+        #     print("Middleware ejecutándose en una solicitud GET")
+        #     singleton.set_nombre_db('default')
+        # else : 
+        #     singleton.set_nombre_db('lectaen')
             
             
 class NombreDBSingleton:
     _instance = None
-    nombre_db = 'lectaen'
+    nombre_db = 'default'
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
