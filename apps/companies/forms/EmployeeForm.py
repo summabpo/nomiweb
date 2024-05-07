7956183e5d9a4ed936675e24a17fd884770aed3d
mikeberdugo <manuel.david.13.b@gmail.com>
2024-05-07 09:02:03 -0500

    .

--- apps/companies/forms/EmployeeForm.py
+++ apps/companies/forms/EmployeeForm.py
@@ -7,39 +7,6 @@ from crispy_forms.layout import Layout, Div, Submit,HTML
 
 
 class EmployeeForm(forms.Form):
-    # identification_type = forms.ChoiceField(choices=[('', '----------')] + [(documento.codigo, documento.documento) for documento in Tipodocumento.objects.using("lectaen").all()], label='Tipo de documento de identidad ')
-    # identification_number = forms.IntegerField(label='Documento de Identidad ')
-    # expedition_date = forms.DateField(label='Fecha de expedición ',widget=forms.DateInput(attrs={'type': 'date'}))
-    # expedition_city = forms.ChoiceField(choices=[('', '----------')] + [(ciudad.idciudad,  f"{ciudad.ciudad} - {ciudad.departamento}" ) for ciudad in Ciudades.objects.using("lectaen").all().order_by('ciudad')], label='Ciudad de expedición ' , widget=forms.Select(attrs={'data-control': 'select2'}) )
-    # first_name = forms.CharField(label='Primer Nombre ')
-    # second_name = forms.CharField(label='Segundo Nombre', required=False)
-    # first_last_name = forms.CharField(label='Primer Apellido ')
-    # second_last_name = forms.CharField(label='Segundo Apellido', required=False)
-    # sex = forms.ChoiceField(choices=[('', '----------'), ('masculino', 'Masculino'), ('femenino', 'Femenino')], label='Sexo ')
-    # height = forms.CharField(label='Estatura (Mts)', required=False)
-    # marital_status = forms.ChoiceField(choices=[('', '----------'), ('soltero', 'Soltero'), ('casado', 'Casado'), ('viudo', 'Viudo'), ('divorciado', 'Divorciado'), ('unionlibre', 'Unión Libre')], label='Estado Civil ')
-    # weight = forms.CharField(label='Peso (Kg)', required=False)
-    # birthdate = forms.DateField(label='Fecha de Nacimiento ',widget=forms.DateInput(attrs={'type': 'date'}))
-    # education_level = forms.ChoiceField(choices=[('', '----------'), ('primaria', 'Primaria'), ('Bachiller', 'Bachiller'), ('bachillerinc', 'Bachiller Incompleto'), ('tecnico', 'Técnico'), ('tecnologo', 'Tecnólogo'), ('universitario', 'Universitario'), ('universitarioinc', 'Universitario Incompleto'), ('postgrado', 'Postgrado'), ('magister', 'Magíster')], label='Nivel Educativo', required=False)
-    # birth_city = forms.ChoiceField(choices=[('', '----------')] + [(ciudad.idciudad,  f"{ciudad.ciudad} - {ciudad.departamento}" ) for ciudad in Ciudades.objects.using("lectaen").all().order_by('ciudad')], label='Ciudad de Nacimiento',widget=forms.Select(attrs={'data-control': 'select2'}) )
-    # stratum = forms.ChoiceField(choices=[('', '----------'), ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6')], label='Estrato', required=False)
-    # birth_country = forms.ChoiceField(choices=[('', '----------')] + [(country.pais, country.pais) for country in Paises.objects.using("lectaen").all()], label='País de Nacimiento' , widget=forms.Select(attrs={'data-control': 'select2'}))
-    # military_id = forms.CharField(label='Libreta Militar', required=False)
-    # blood_group = forms.ChoiceField(choices=[('', '-----'), ('OP', 'O +'), ('ON', 'O -'), ('AN', 'A -'), ('AP', 'A +'), ('BP', 'B +'), ('BN', 'B -'), ('ABP', 'AB +'), ('ABN', 'AB -')], label='Grupo Sanguíneo', required=False)
-    # profession = forms.ChoiceField(choices=[('', '----------')] + [(profesion.idprofesion, profesion.profesion) for profesion in Profesiones.objects.using("lectaen").all()], label='Profesión', required=False , widget=forms.Select(attrs={'data-control': 'select2'}) )
-    # residence_address = forms.CharField(label='Dirección de Residencia')
-    # email = forms.EmailField(label='E-mail')
-    # residence_city = forms.ChoiceField(choices=[('', '----------')] + [(ciudad.idciudad,  f"{ciudad.ciudad} - {ciudad.departamento}" ) for ciudad in Ciudades.objects.using("lectaen").all().order_by('ciudad')], label='Ciudad de Residencia',widget=forms.Select(attrs={'data-control': 'select2'}))
-    # cell_phone = forms.CharField(label='Celular')
-    # residence_country = forms.ChoiceField(choices=[('', '----------')] + [(country.pais, country.pais) for country in Paises.objects.using("lectaen").all()], label='País de residencia' , widget=forms.Select(attrs={'data-control': 'select2'}))
-    # employee_phone = forms.CharField(label='Teléfono del Empleado', required=False)
-    # pants_size = forms.ChoiceField(choices=[('', '----------'), ('6', '6'), ('8', '8'), ('10', '10'), ('12', '12'), ('14', '14'), ('16', '16'), ('28', '28'), ('30', '30'), ('32', '32'), ('34', '34'), ('36', '36'), ('38', '38'), ('40', '40')], label='Talla Pantalón',required=False)
-    # shirt_size = forms.ChoiceField(choices=[('', '----------'), ('38', '38'), ('40', '40'), ('42', '42'), ('44', '44'), ('XS', 'XS'), ('S', 'S'), ('M', 'M'), ('L', 'L'), ('XL', 'XL'), ('XXL', 'XXL')], label='Talla Camisa' , required=False)
-    # shoes_size = forms.ChoiceField(choices=[('', '----------'), ('34', '34'), ('35', '35'), ('36', '36'), ('37', '37'), ('38', '38'), ('39', '39'), ('40', '40'), ('41', '41'), ('42', '42'), ('43', '43'), ('44', '44')], label='Talla Zapatos', required=False)
-
-
-
-            
     def clean(self):
         cleaned_data = super().clean()
         first_name = cleaned_data.get('first_name')
