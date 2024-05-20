0b1e4a8274377e1450b854ab42cd294c04dbfb3e
mikeberdugo <manuel.david.13.b@gmail.com>
2024-05-20 17:14:26 -0500

    Cierre 20/05/2024
    Se genero el filtrado dinámico para la planilla de nomina , falta verificación de datos de la tabla de visualización

---
+++ apps/companies/views/payrollsheet/payrollsheet.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from apps.companies.models import Nomina
+import random
+from datetime import datetime, timedelta
+
+def payrollsheet(request):
+    nominas = Nomina.objects.select_related('idnomina').values_list('idnomina__nombrenomina', 'idnomina').distinct()
+    compects = []  # Define compects here
+
+    if request.method == 'POST':
+        nomina_id = request.POST.get('nomina_select')
+        compectos = Nomina.objects.filter(idnomina = nomina_id )
+        
+        
+        
+        for data in compectos:
+            
+            compects.append({
+                'documento': data.idempleado.docidentidad,
+                'nombre': f"{data.idempleado.papellido} {data.idempleado.sapellido} {data.idempleado.pnombre} {data.idempleado.snombre}",
+                'basico': data.valor ,
+                'cargo': 'prueba',
+                'centrocostos': 'prueba',
+                'tipocontrato': 'prueba',
+                'tarifaARL': 'prueba'
+            })
+        
+    # No need for else here, compects will be an empty list if it's not a POST request
+    
+    return render(request, 'companies/payrollsheet.html', {
+        'nominas': nominas,
+        'compects': compects,
+    })
