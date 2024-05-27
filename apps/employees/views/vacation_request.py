ae2d7fd0365a123cad4967c213d6c072c6adf1a5
GUIDO ANGULO <sat1124@proton.me>
2024-05-27 11:51:10 -0500

    VAC REQUEST FORM JS

--- apps/employees/views/vacation_request.py
+++ apps/employees/views/vacation_request.py
@@ -1,30 +1,33 @@
 from django.shortcuts import render, redirect
-from django.views.generic import  ListView
-from django.db.models import Q
+from django.db.models import Q, Sum
 from apps.employees.forms.vacation_request_form import EmpVacacionesForm
-#models
-from apps.employees.models import EmpVacaciones
+# models
+from apps.employees.models import EmpVacaciones, Vacaciones
 
-class VacationRequestList(ListView):
-    template_name = 'employees/vacations_request.html'
-    paginate_by = 10
-    context_object_name = 'vacation_request_list'
-    model = EmpVacaciones
-    ordering = 'id_sol_vac'
-    
-    def get_queryset(self):
-        queryset = EmpVacaciones.objects.filter(Q(idcontrato=3778) & (Q(tipovac=1) | Q(tipovac=2) | Q(tipovac= 3) | Q(tipovac=4))).select_related('tipovac')
-        return queryset
-   # return render(request, 'employees/vacations_request_list.html')
+idc = 3778
 
-def vacations_request(request):
+
+def vacation_request_function(request):
+    idc = 3778  # Obtén el ID del contrato como corresponda
     if request.method == 'POST':
         form = EmpVacacionesForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('vacation_request_list')
+            return redirect('employees:form_vac')
     else:
         form = EmpVacacionesForm()
 
-    return render(request, 'employees/vacations_request.html', {'form': form})
+    # Obtener la suma de días de vacaciones disfrutadas
+    resultado_vacaciones = Vacaciones.objects.filter(idcontrato=idc).aggregate(dias_vacaciones=Sum('diasvac'))
+    dias_vacaciones = resultado_vacaciones['dias_vacaciones'] if resultado_vacaciones['dias_vacaciones'] is not None else 0
 
+    vacation_list = EmpVacaciones.objects.all()
+    
+    context = {
+        'form': form,
+        'dias_vacaciones': dias_vacaciones,
+        'vacation_list': vacation_list,
+        'idc': idc,
+    }
+
+    return render(request, 'employees/vacations_request.html', context)
