9210de7c45e3635bf15888bea59e7467e8c6806c
GUIDO ANGULO <sat1124@proton.me>
2024-05-27 23:19:14 -0500

    asignada variable idc a partir de idempleado de session

--- apps/employees/views/vacation_request.py
+++ apps/employees/views/vacation_request.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 from django.db.models import Sum
 from apps.employees.forms.vacation_request_form import EmpVacacionesForm
-from apps.employees.models import EmpVacaciones, Vacaciones
+from apps.employees.models import EmpVacaciones, Vacaciones, Contratos
 
 def vacation_request_function(request):
-    idc = request.session.get('idcontrato')
+    ide = request.session.get('idempleado')
+
+    contrato = Contratos.objects.filter(idempleado=ide, estadocontrato=1).first()
+    if contrato:
+        idc = contrato.idcontrato
+    else:
+        idc = None
+
     form = EmpVacacionesForm(request.POST or None)
 
     if request.method == 'POST' and form.is_valid():
@@ -13,7 +20,7 @@ def vacation_request_function(request):
 
     dias_vacaciones = Vacaciones.objects.filter(idcontrato=idc).aggregate(Sum('diasvac'))['diasvac__sum'] or 0
 
-    vacation_list = EmpVacaciones.objects.all()
+    vacation_list = EmpVacaciones.objects.filter(idcontrato=idc).order_by('-id_sol_vac')
 
     context = {
         'form': form,
