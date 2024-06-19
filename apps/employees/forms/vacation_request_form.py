f021fee2359adf0e458a5052a833517014199a12
mikeberdugo <manuel.david.13.b@gmail.com>
2024-06-19 10:07:13 -0500

    .

--- apps/employees/forms/vacation_request_form.py
+++ apps/employees/forms/vacation_request_form.py
@@ -61,7 +61,6 @@ class EmpVacacionesForm(forms.ModelForm):
                 Column('diasvac', css_class='col-md-6 mb-3', id='diasvac-column'),
                 Column('comentarios', css_class='col-md-6 mb-3'),
             ),
-            Submit('submit', 'Guardar',css_class='btn btn-light-info hover-elevate-up')
         )
         
     def label_from_contrato(self, obj):
