6107cce63ce6ca5b6ba91908f4680aab8e35b415
mikeberdugo <manuel.david.13.b@gmail.com>
2024-06-20 14:08:25 -0500

    .

--- nomiweb/db_routers/routers.py
+++ nomiweb/db_routers/routers.py
@@ -1,15 +1,44 @@
+from django.db import connections, OperationalError
+from apps.login.middlewares import NombreDBSingleton
+from functools import lru_cache
+
 class DatabaseRouter:
+    def __init__(self):
+        self.nombre_db_singleton = NombreDBSingleton()
+
+    @lru_cache(maxsize=None)
+    def table_exists(self, table_name, db_alias='default'):
+        """
+        Verifica si una tabla existe en la base de datos especificada.
+        """
+        try:
+            with connections[db_alias].cursor() as cursor:
+                cursor.execute("SELECT * FROM information_schema.tables WHERE table_name = %s", [table_name])
+                return bool(cursor.fetchone())
+        except OperationalError:
+            return False
+
+    def _determine_db(self, model):
+        """
+        Determina el nombre de la base de datos a utilizar para una operación.
+        """
+        nombre_db = self.nombre_db_singleton.get_nombre_db()
+        if self.table_exists(model._meta.db_table):
+            return 'default'
+        else:
+            return nombre_db
+
     def db_for_read(self, model, **hints):
         """
-        Retorna el nombre de la base de datos a utilizar para operaciones de lectura.
+        Devuelve el nombre de la base de datos a utilizar para operaciones de lectura.
         """
-        return 'default'
+        return self._determine_db(model)
 
     def db_for_write(self, model, **hints):
         """
-        Retorna el nombre de la base de datos a utilizar para operaciones de escritura.
+        Devuelve el nombre de la base de datos a utilizar para operaciones de escritura.
         """
-        return 'default'
+        return self._determine_db(model)
 
     def allow_relation(self, obj1, obj2, **hints):
         """
@@ -21,4 +50,4 @@ class DatabaseRouter:
         """
         Determina si las migraciones deben ser aplicadas en una base de datos particular.
         """
-        return db == 'default'
+        return True
--- nomiweb/settings.py
+++ nomiweb/settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'apps.login.middlewares.DatabaseRouterMiddleware',
+    'apps.login.middlewares.DatabaseRouterMiddleware',
     #'allauth.account.middleware.AccountMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
