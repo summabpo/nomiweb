c162a070a6eeef5c00dd8d010616a12983f81a0c
GUIDO ANGULO <sat1124@proton.me>
2024-07-04 18:12:28 -0500

    Ajustes dotenv - wsgi - S3

--- nomiweb/settings/base.py
+++ nomiweb/settings/base.py
@@ -178,21 +178,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '..', 'static')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'staticfiles')
-
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- nomiweb/settings/production.py
+++ nomiweb/settings/production.py
@@ -1,11 +1,13 @@
 from .base import *
+from boto3.session import Session
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =  os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+DEBUG = False
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+print("ALLOWED_HOSTS:", os.getenv('ALLOWED_HOSTS'))
 
 SETTINGS_ENV = 'production'
 
@@ -36,3 +38,36 @@ DATABASES = {
         'PORT':  os.getenv('DB_PORT_PROD'),
     }
 }
+
+# Seguridad de cookies
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+print("AWS_ACCESS_KEY_ID:", os.getenv('AWS_ACCESS_KEY_ID'))
+print("AWS_SECRET_ACCESS_KEY:", os.getenv('AWS_SECRET_ACCESS_KEY'))
+print("AWS_STORAGE_BUCKET_NAME:", os.getenv('AWS_STORAGE_BUCKET_NAME'))
+print("AWS_S3_REGION_NAME:", os.getenv('AWS_S3_REGION_NAME'))
+CSRF_COOKIE_HTTPONLY = False
+CSRF_TRUSTED_ORIGINS = ['localhost', '127.0.0.1']
+
+
+# Configuración de S3
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Almacén estático y de medios
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# URL estática y de medios
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+# Configuración de caché
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE = False
+
+#STATIC_ROOT = os.path.join(BASE_DIR, '..', 'staticfiles')
