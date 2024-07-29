48b2de956ed6038653e6c8faac3a15b654c5db00
mikeberdugo <manuel.david.13.b@gmail.com>
2024-07-29 09:31:44 -0500

    Descargade documento
    Descarga Excel de acumulados de nomina

--- apps/companies/views/payrollaccumulations/payrollaccumulations.py
+++ apps/companies/views/payrollaccumulations/payrollaccumulations.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from apps.components.generate_employee_excel import generate_employee_excel
 from apps.components.humani import format_value
+from django.http import JsonResponse
 
 
 def payrollaccumulations(request):
@@ -78,38 +79,66 @@ def payrollaccumulations(request):
     
     
 def descargar_excel_empleados(request):
-    acumulados = {}
-    # Diccionario de ejemplo con 6 empleados y datos asociados
-    nominas = Nomina.objects.all()[:70] ## .order_by('idconcepto__idconcepto')
-    
-    for data in nominas:
-            docidentidad = data.idempleado.docidentidad
-            if docidentidad not in acumulados:
-                acumulados[docidentidad] = {
-                    'documento': docidentidad,
-                    'Empleado': f"{data.idempleado.papellido} {data.idempleado.sapellido} {data.idempleado.pnombre} {data.idempleado.snombre} - {data.idempleado.docidentidad} - {data.idcontrato.idcontrato} ",
-                    'total':0,
-                    'data':[
-                        {"idconcepto": data.idconcepto.idconcepto,
-                        "nombreconcepto": data.nombreconcepto, 
-                        "cantidad": data.cantidad, 
-                        "valor": data.valor},
-                    ]
-                }
-            else:
-                nuevo_concepto = {"idconcepto": data.idconcepto.idconcepto, 
-                                "nombreconcepto": data.nombreconcepto,
-                                "cantidad": data.cantidad,  
-                                "valor": data.valor }
-                
-                acumulados[docidentidad]["data"].append(nuevo_concepto)
-            acumulados[docidentidad]['total'] += data.valor
+    if request.method == 'POST':
+        acumulados = {}
+        print('entre')
+        # Obtener parámetros del POST
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+        employee = request.POST.get('employee')
+        cost_center = request.POST.get('cost_center')
+        city = request.POST.get('city')
+        
+        # Aplicar filtros a la consulta de Nomina
+        filtros = {}
+        
+        if employee:
+            filtros['idempleado__idempleado'] = employee
+        if cost_center:
+            filtros['idcontrato__idcosto'] = cost_center
+        if city:
+            filtros['idcontrato__idsede'] = city
+        if start_date:
+            filtros['idnomina__fechainicial__gte'] = start_date
+        if end_date:
+            filtros['idnomina__fechafinal__lte'] = end_date
+        
+        nominas = Nomina.objects.filter(**filtros)
             
-    # Generar el archivo Excel
-    output = generate_employee_excel(acumulados)
+        
+        
+        for data in nominas:
+                docidentidad = data.idempleado.docidentidad
+                if docidentidad not in acumulados:
+                    acumulados[docidentidad] = {
+                        'documento': docidentidad,
+                        'Empleado': f"{data.idempleado.papellido} {data.idempleado.sapellido} {data.idempleado.pnombre} {data.idempleado.snombre} - {data.idempleado.docidentidad} - {data.idcontrato.idcontrato} ",
+                        'total':0,
+                        'data':[
+                            {"idconcepto": data.idconcepto.idconcepto,
+                            "nombreconcepto": data.nombreconcepto, 
+                            "cantidad": data.cantidad, 
+                            "valor": data.valor},
+                        ]
+                    }
+                else:
+                    nuevo_concepto = {"idconcepto": data.idconcepto.idconcepto, 
+                                    "nombreconcepto": data.nombreconcepto,
+                                    "cantidad": data.cantidad,  
+                                    "valor": data.valor }
+                    
+                    acumulados[docidentidad]["data"].append(nuevo_concepto)
+                acumulados[docidentidad]['total'] += data.valor
+          
+          
+        print(acumulados)      
+        # Generar el archivo Excel
+        output = generate_employee_excel(acumulados)
+        
+        # Crear una respuesta HTTP con el archivo Excel
+        response = HttpResponse(output, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename="empleado_info.xlsx"'
+        print(response)
+        return response
     
-    # Crear una respuesta HTTP con el archivo Excel
-    response = HttpResponse(output, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    response['Content-Disposition'] = 'attachment; filename="empleado_info.xlsx"'
-    
-    return response
+    return JsonResponse({'error': 'Método no permitido'}, status=405)
