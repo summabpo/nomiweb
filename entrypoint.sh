4462e333ad5a44f6beb1d1e1d191d0a2ed0705ee
GUIDO ANGULO <sat1124@proton.me>
2024-08-23 23:32:33 -0500

    update compose y commen collectstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ python manage.py migrate --noinput
 
 # # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+#python manage.py collectstatic --noinput
 
 # Run server with Gunicorn
 echo 'Running server...'
