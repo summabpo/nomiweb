5f457acc2a0b67be4b22523fd859777753c217bc
GUIDO ANGULO <sat1124@proton.me>
2024-08-26 11:37:56 -0500

    upd production

--- nomiweb/settings/production.py
+++ nomiweb/settings/production.py
@@ -35,6 +35,14 @@ DATABASES = {
         'PASSWORD':  os.getenv('DB_PASSWORD_PROD'),
         'HOST':  os.getenv('DB_HOST_PROD'),
         'PORT':  os.getenv('DB_PORT_PROD'),
+    },
+    'nwp_match': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nwp_match',
+        'USER':  os.getenv('DB_USER_PROD'),
+        'PASSWORD':  os.getenv('DB_PASSWORD_PROD'),
+        'HOST':  os.getenv('DB_HOST_PROD'),
+        'PORT':  os.getenv('DB_PORT_PROD'),
     }
 }
 
