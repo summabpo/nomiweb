42dd30ebc5fe532303b5785d3d74e22259fe2da4
manuelberdugo13 <166764966+manuelberdugo13@users.noreply.github.com>
2025-01-16 21:52:02 +0000

    modified:   apps/payroll/views/parameters/forms.py modified:   apps/payroll/views/parameters/parameters.py modified:   apps/payroll/views/payroll/payroll.py modified:   templates/payroll/entities.html modified:   templates/payroll/payrollviews.html

--- apps/payroll/views/parameters/parameters.py
+++ apps/payroll/views/parameters/parameters.py
@@ -53,29 +53,42 @@ def banks(request):
 @login_required
 @role_required('accountant')
 def entities(request):
-    usuario = request.session.get('usuario', {})
-    idempresa = usuario['idempresa']
+
     form = EntitiesForm()
-    entidades = Entidadessegsocial.objects.all().order_by('entidad')
+    entidades = Entidadessegsocial.objects.all().exclude(
+        codigo__in=['000', '9999', '9988', '9998']
+    ).exclude(
+        nit=''
+    ).exclude(
+        nit__isnull=True
+    ).order_by('entidad')
     error = False
 
     if request.method == 'POST':
         form = EntitiesForm(request.POST)
         if form.is_valid():
             try:
-                
+                # Validar si el código ya existe
+                codigo = form.cleaned_data['codigo']
+                if Entidadessegsocial.objects.filter(codigo=codigo).exists():
+                    print('codigo',codigo)
+                    messages.error(request, f"El código {codigo} ya existe en la base de datos.")
+                    return redirect('payroll:entities')
 
-                # Crear instancia de Crearnomina
+                # Crear instancia de Entidadessegsocial
                 Entidadessegsocial.objects.create(
-                    codigo=form.cleaned_data['codigo'],
+                    codigo=codigo,
                     nit=form.cleaned_data['nit'],
-                    entidad=form.cleaned_data['entidad']
+                    entidad=form.cleaned_data['entidad'],
+                    tipoentidad=form.cleaned_data['tipoentidad'],
+                    codsgp=form.cleaned_data['codsgp'] if form.cleaned_data['codsgp'] else None,
                 )
 
-                messages.success(request, "Nómina creada exitosamente.")
-                return redirect('payroll:payroll')  # Redirigir a una vista de lista, por ejemplo
-            except :
+                messages.success(request, "Entidad creada exitosamente.")
+                return redirect('payroll:entities')  # Redirigir a una vista de lista, por ejemplo
+            except ValueError as e:
                 messages.error(request, "Hubo un problema al procesar la información.")
+                return redirect('payroll:entities')
 
         else:
             error = True
@@ -86,9 +99,8 @@ def entities(request):
                     error_message += f"\n- {error}"
 
             messages.error(request, error_message)
-    
-    return render(request, './payroll/entities.html', {'entidades': entidades, 'form': form, 'error': error})
 
+    return render(request, './payroll/entities.html', {'entidades': entidades, 'form': form, 'error': error})
 
 
 @login_required
