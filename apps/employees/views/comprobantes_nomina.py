12bf796e6f7ef4968b3bb01a68a6faef4ed49967
mikeberdugo13 <manuel.berdugo@summabpo.com>
2025-07-24 08:12:58 -0500

    fix/hu-460

--- apps/employees/views/comprobantes_nomina.py
+++ apps/employees/views/comprobantes_nomina.py
@@ -12,6 +12,18 @@ from apps.components.payrollgenerate import genera_comprobante
 from apps.components.decorators import  role_required
 from django.contrib.auth.decorators import login_required
 
+from apps.components.decorators import  role_required
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.utils import ImageReader
+from reportlab.platypus import Table, TableStyle
+from io import BytesIO
+from reportlab.lib import colors
+from reportlab.lib.units import cm
+import time
+from reportlab.pdfgen import canvas
+
 # Create your views here.
 from django.views.generic import  ListView
 
@@ -101,33 +113,233 @@ def listaNomina(request):
 @login_required
 @role_required('employee')
 def generatepayrollcertificate(request ,idnomina,idcontrato,):
-    """
-    Genera un certificado de nómina en formato PDF para un empleado y contrato específicos.
+    
+    context = genera_comprobante(idnomina, idcontrato)
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer, pagesize=letter)
+    width, height = letter
+    # Encabezado empresa
+    p.setFont("Helvetica-Bold", 8)
+    p.drawCentredString(width / 2, height - 25, context['empresa'])
+    p.setFont("Helvetica", 8)
+    p.drawCentredString(width / 2, height - 35, context['nit'])
+    p.drawCentredString(width / 2, height - 45, context['web'])
 
-    Esta vista toma los datos de una nómina y contrato, los utiliza para generar un comprobante de nómina en formato PDF.
-    El PDF se genera utilizando una plantilla HTML y se envía al navegador como un archivo descargable.
+    # Línea horizontal
+    p.setStrokeColor(colors.grey)
+    p.setLineWidth(0.5)
+    p.line(35, height - 60, width - 35, height - 60)
 
-    Parameters
-    ----------
-    request : HttpRequest
-        Solicitud HTTP del navegador. Contiene los parámetros POST necesarios para generar el certificado.
-    idnomina : int
-        ID de la nómina para la que se generará el certificado.
-    idcontrato : int
-        ID del contrato asociado con la nómina.
+    # Logo
+    try:
+        logo = ImageReader(f"static/img/{context['logo']}")
+        logo_width = 150
+        logo_height = 50
+        p.drawImage(logo, 35, height - 55, width=logo_width, height=logo_height,
+                    preserveAspectRatio=True, mask='auto')
+    except:
+        pass
 
-    Returns
-    -------
-    HttpResponse
-        PDF del certificado de nómina generado.
+    # Subtítulo
+    p.setFont("Courier-Bold", 15)
+    p.drawCentredString(width / 2, height - 90, "Comprobante de Nómina")
 
-    Notes
-    -----
-    - Solo accesible para empleados autenticados.
-    - El nombre del archivo PDF incluye el nombre del contrato y la fecha actual.
-    - En caso de error al generar el PDF, se muestra un mensaje de error y el código de estado 400.
-    """
+    y = height - 120
+# ---------------------- Tabla 1: Empleado ----------------------
+    tabla1 = [
+        ["Empleado", "Identificación", "Contrato", "Nómina"],
+        [context['nombre_completo'], context['cc'], context['idcon'], context['idnomi']]
+    ]
 
+    tabla = Table(tabla1, colWidths=[8*cm, 4*cm, 4*cm, 4*cm], rowHeights=20)
+    tabla.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor("#b9c1c4")),
+        ('GRID', (0, 0), (-1, -1), 0.5, colors.black),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER')
+    ]))
+    tabla.wrapOn(p, width, y)
+    tabla.drawOn(p, 22, y - 40)
+    y -= 40
+
+    # ---------------------- Tabla 2: Cargo y salario ----------------------
+    tabla2 = [
+        ["Fecha Ingreso", "Cargo", "Salario", "Cuenta"],
+        [context['fecha1'], context['cargo'], context['salario'], context['cuenta']]
+    ]
+
+    tabla = Table(tabla2, colWidths=[4*cm, 8*cm, 4*cm, 4*cm], rowHeights=20)
+    tabla.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor("#b9c1c4")),
+        ('GRID', (0, 0), (-1, -1), 0.5, colors.black),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER')
+    ]))
+    tabla.wrapOn(p, width, y)
+    tabla.drawOn(p, 22, y - 40)
+    y -= 40
+
+    # # ---------------------- Tabla 3: Costos y entidades ----------------------
+    tabla3 = [
+        ["Centro de Costo", "Periodo de Pago", "Salud", "Pensión"],
+        [context['ccostos'], context['periodos'], str(context['eps']), str(context['pension'])]
+    ]
+
+    tabla = Table(tabla3, colWidths=[5*cm, 8*cm, 3.5*cm, 3.5*cm], rowHeights=20)
+    tabla.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor("#b9c1c4")),
+        ('GRID', (0, 0), (-1, -1), 0.5, colors.black),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER')
+    ]))
+    tabla.wrapOn(p, width, y)
+    tabla.drawOn(p, 22, y - 40)
+
+
+
+    # # ---------------------- Tabla 4: Ingresos y Deducciones ----------------------
+    
+    encabezado_ingresos = [['INGRESOS', '', '', '']]
+    filas_ingresos = []
+
+    devengados = context['dataDevengado']
+    for ingreso in devengados:
+        fila = [
+            ingreso.idconcepto.codigo,
+            ingreso.nombreconcepto if ingreso else '',
+            ingreso.cantidad if ingreso else '',
+            f"{str(ingreso.valor).replace('.', ',')}" if ingreso else '',
+        ]
+        filas_ingresos.append(fila)
+
+    # Rellenar hasta 15 filas (sin contar el encabezado)
+    while len(filas_ingresos) < 15:
+        filas_ingresos.append(['', '', '', ''])
+
+    tabla_ingresos = encabezado_ingresos + filas_ingresos
+
+    # DEDUCCIONES
+    encabezado_descuentos = [['DEDUCCIONES', '', '', '']]
+    filas_descuentos = []
+
+    descuentos = context['dataDescuento']
+    for descuento in descuentos:
+        fila = [
+            descuento.idconcepto.codigo,
+            descuento.nombreconcepto if descuento else '',
+            descuento.cantidad if descuento else '',
+            f"{str(descuento.valor).replace('.', ',')}" if descuento else '',
+        ]
+        filas_descuentos.append(fila)
+
+    # Rellenar hasta 15 filas
+    while len(filas_descuentos) < 15:
+        filas_descuentos.append(['', '', '', ''])
+
+    tabla_descuentos = encabezado_descuentos + filas_descuentos
+
+    # Columnas
+    col_widths = [1*cm, 5*cm, 2*cm, 2*cm]
+
+    # Crear tablas
+    table_ingresos = Table(tabla_ingresos, colWidths=col_widths)
+    table_descuentos = Table(tabla_descuentos, colWidths=col_widths)
+
+    # Estilo común
+        
+    estilo_tabla = TableStyle([
+        ('SPAN', (0, 0), (-1, 0)),
+        # Encabezado gris claro sin colores si prefieres: eliminalo
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor("#b9c1c4")),
+        
+        ('FONTNAME', (0, 0), (-1, 0), 'Courier-Bold'),
+        ('FONTNAME', (0, 1), (-1, -1), 'Courier'),
+        ('FONTSIZE', (0, 0), (-1, -1), 9),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+
+        # Alineación general
+        ('ALIGN', (1, 1), (1, -1), 'LEFT'),# Alinear concepto a la izquierda
+        ('ALIGN', (2, 1), (2, -1), 'RIGHT'),# Alinear cantidad a la derecha
+        ('ALIGN', (3, 1), (3, -1), 'RIGHT'), # Alinear valor a la derecha
+
+        # Si no quieres ninguna línea ni color:
+        # (simplemente omite las siguientes líneas o comenta)
+        ('LINEBEFORE', (1, 0), (-1, -1), 0.5, colors.black),
+        ('LINEAFTER', (0, 0), (-2, -1), 0.5, colors.black),
+        ('BOX', (0, 0), (-1, -1), 0.5, colors.black),
+    ])
+    table_ingresos.setStyle(estilo_tabla)
+    table_descuentos.setStyle(estilo_tabla)
+
+    # Posiciones
+    w_i, h_i = table_ingresos.wrap(0, 0)
+    w_d, h_d = table_descuentos.wrap(0, 0)
+
+    bottom_y = height - 250
+    table_ingresos.drawOn(p, x=22, y=bottom_y - h_i)
+    table_descuentos.drawOn(p, x=306, y=bottom_y - h_d)
+    
+
+    # ---------------------- Tabla 5: Totales ----------------------
+
+    tabla5 = [
+        ['', f"Total Ingresos:", '', context['sumadataDevengado'], '', f"Total Deducciones: ", '', context['sumadataDescuento']],
+        ['', 'Total a Pagar:', '', context['total'], '', '', '', '']
+    ]
+
+    col_widths = [1*cm, 5*cm, 1*cm, 3*cm, 1*cm, 5*cm, 1*cm, 3*cm]
+
+    tabla = Table(tabla5, colWidths=col_widths, rowHeights=18)
+
+    tabla.setStyle(TableStyle([
+        ('BOX', (0, 0), (-1, -1), 0.8, colors.black),
+
+        # Encabezado con spans bien colocado
+        # Colores
+        ('BACKGROUND', (0, 0), (3, 0), colors.HexColor("#b9c1c4")),
+        ('BACKGROUND', (4, 0), (7, 0), colors.HexColor("#b9c1c4")),
+        ('BACKGROUND', (0, -1), (2, -1), colors.HexColor("#81DAF5")),
+
+        ('FONTNAME', (0, 0), (-1, 1), 'Courier-Bold'),
+        ('FONTSIZE', (0, 1), (-1, 1), 12),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        
+        
+        ('ALIGN', (3, 0), (3, 0), 'RIGHT'),  # sumadataDevengado
+        ('ALIGN', (7, 0), (7, 0), 'RIGHT'),  # sumadataDescuento
+        ('ALIGN', (3, 1), (3, 1), 'RIGHT'),  # total
+    
+    ]))
+
+    tabla.wrapOn(p, width, y)
+    tabla.drawOn(p, 22, y - 390)
+    
+    
+    # Footer institucional
+    p.setFont("Helvetica", 8)
+    p.setFillColor(colors.grey)
+    p.drawCentredString(width / 2, 15, "Outsourcing de Nómina ::: www.nomiweb.co ::: Summa BPO SAS")
+
+
+    ## data unica 
+    fecha_actual = datetime.now().strftime('%Y-%m-%d')
+    nombre_archivo = f'Certificado_{context["cc"]}_{fecha_actual}.pdf'
+    
+    # Finalizar PDF
+    p.showPage()
+    p.setTitle(nombre_archivo)
+    p.setAuthor("Nomiweb")
+    p.setSubject("Comprobante de Nomina {context['cc']}")
+    p.setCreator("Sistema Nomiweb")
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'inline; filename="{nombre_archivo}"'
+    response.write(pdf)
+    return response
+    
     context = genera_comprobante(idnomina,idcontrato)
 
     html_string = render(request, './html/payrollcertificate.html', context).content.decode('utf-8')
