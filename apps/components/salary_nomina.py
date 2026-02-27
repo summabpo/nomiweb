e0f3ea55692dc3261f5ce081ece8e78bbab1a555
manuelberdugo13 <manuel.berdugo@summabpo.com>
2026-02-27 17:10:45 -0500

    modified:   apps/components/salary_nomina.py modified:   apps/payroll/views/payroll/payroll_automatic_systems.py

--- apps/components/salary_nomina.py
+++ apps/components/salary_nomina.py
@@ -1,27 +1,67 @@
 from apps.common.models import Crearnomina , HistoricoNomina ,NominaComprobantes, NovSalarios , Empresa , Anos , Nomina , Contratos
+from decimal import Decimal
 
 
 
+def salary_nomina_update(contrato, fecha_inicio, fecha_fin):
 
-def salary_nomina_update(contrato,fecha):
-    salary = contrato.salario
+    estructura = []
 
-    data = NovSalarios.objects.filter( idcontrato = contrato ).order_by('-idcambiosalario').first()
-    nomina = Nomina.objects.filter(idnomina__fechapago__lt=data.fechanuevosalario , idnomina__tiponomina__idtiponomina__in=[1, 2] ,idconcepto__codigo = 1, idnomina__id_empresa = contrato.id_empresa).order_by('-idnomina').first()
+    salario_actual = contrato.salario
+
+    print("\n========= ANALISIS CAMBIO SALARIAL =========")
+    print("Periodo:", fecha_inicio, "->", fecha_fin)
+    print("Salario actual contrato:", salario_actual)
+
+    cambio = NovSalarios.objects.filter(
+        idcontrato=contrato,
+        fechanuevosalario__gt=fecha_inicio,
+        fechanuevosalario__lte=fecha_fin
+    ).order_by('fechanuevosalario').first()
+
+    # 🔹 Caso 1: No hay cambio dentro del periodo
+    if not cambio:
+        dias_totales = (fecha_fin - fecha_inicio).days + 1
+
+        estructura.append({
+            "dias": dias_totales,
+            "salario_mensual": salario_actual,
+            "salario_dia": salario_actual / Decimal(30)
+        })
+
+
+        return estructura
+
+    # 🔹 Caso 2: Sí hubo cambio dentro del periodo
+
+    salario_anterior = cambio.salarioactual
+    fecha_cambio = cambio.fechanuevosalario
+
+
+    dias_anteriores = (fecha_cambio - fecha_inicio).days
+    dias_nuevos = (fecha_fin - fecha_cambio).days + 1
 
 
 
+    # Tramo 1
+    if dias_anteriores > 0:
+        estructura.append({
+            "dias": dias_anteriores,
+            "salario_mensual": salario_anterior,
+            "salario_dia": salario_anterior / 30
+        })
 
-    print('-------------------')
-    print(salary)
-    print(data)
-    print(data.fechanuevosalario)
-    print(nomina.valor)
-    print(type(fecha))
-    print('-------------------')
+    # Tramo 2
+    estructura.append({
+        "dias": dias_nuevos,
+        "salario_mensual": salario_actual,
+        "salario_dia": salario_actual /30
+    })
 
+    print("Estructura generada:", estructura)
+    print("============================================\n")
 
-    return salary
+    return estructura
 
 
 
